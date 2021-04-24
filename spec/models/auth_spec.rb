3487138d8a795ae035171ee979f7fc6427871ff9
tpdogan <pelintamerdogan@gmail.com>
2021-04-25 01:50:26 +0200

    auth tests bugs is fixed

--- spec/models/auth_spec.rb
+++ spec/models/auth_spec.rb
@@ -1,22 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Auth, type: :model do
+  let (:user) { User.create(username: 'username', password: 'password') }
+  let (:client) { Client.create(client_name: 'client') }
+  let (:userApp) {UserApp.create(user_id: user.id, client_id: client.id)}
+  let (:auth) {userApp.create_auth()}
+
   context 'generation tests' do
     it 'should generate an auth_code' do
-      auth = Auth.create()
       expect(auth.auth_code).not_to eq('')
       expect(auth.auth_code).not_to eq(nil)
     end
   end
 
   context 'association tests' do
-    let (:user) { User.create(username: 'username', password: 'password') }
-    let (:client) { Client.create(client_name: 'client') }
-    let (:auth) {Auth.create()}
-    let (:userApp) {UserApp.create(user_id: user.id, client_id: client.id, auth_id: auth.id)}
-
     it 'should belong to a userApp' do
-      expect(auth.userApp).to eq(userApp)
+      expect(UserApp.find(auth.user_app_id)).to eq(userApp)
     end
   end
 end
