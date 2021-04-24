74bd2b21ddf09f3eb4b590673d9473ef61accd9d
tpdogan <pelintamerdogan@gmail.com>
2021-04-24 12:56:02 +0200

    validation tests are added for client

--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Client, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validation test' do
+    it 'should have a name' do
+      client = Client.new().save
+      expect(client).to eq(false)
+    end
+
+    it 'should save successfully with a name' do
+      client = Client.new(client_name: 'client').save
+      expect(client).to eq(true)
+    end
+  end
 end
