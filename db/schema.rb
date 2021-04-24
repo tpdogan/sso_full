2d3b43a8958a8a7bb111913dca846339446418e9
tpdogan <pelintamerdogan@gmail.com>
2021-04-24 12:50:17 +0200

    clients table is migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_24_103649) do
+ActiveRecord::Schema.define(version: 2021_04_24_104134) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "clients", force: :cascade do |t|
+    t.string "client_name"
+    t.string "client_id"
+    t.string "client_secret"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
