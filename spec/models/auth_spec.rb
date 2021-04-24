2df4b17b219f59ebcde4094450cc2308b308e667
tpdogan <pelintamerdogan@gmail.com>
2021-04-24 14:19:03 +0200

    auth generation test bug is fixed

--- spec/models/auth_spec.rb
+++ spec/models/auth_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Auth, type: :model do
   context 'generation tests' do
     it 'should generate an auth_code' do
-      auth = Auth.new()
+      auth = Auth.create()
       expect(auth.auth_code).not_to eq('')
       expect(auth.auth_code).not_to eq(nil)
     end
