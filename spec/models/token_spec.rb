5219d3b682352911583f7f476986bbd01b48f3e0
tpdogan <pelintamerdogan@gmail.com>
2021-04-24 20:36:26 +0200

    association test is added for token

--- spec/models/token_spec.rb
+++ spec/models/token_spec.rb
@@ -12,4 +12,15 @@ RSpec.describe Token, type: :model do
       expect(token.token_type).to eq('bearer')
     end
   end
+
+  context 'association tests' do
+    let (:user) { User.create(username: 'username', password: 'password') }
+    let (:client) { Client.create(client_name: 'client') }
+    let (:token) {Token.create()}
+    let (:userApp) {UserApp.create(user_id: user.id, client_id: client.id, token_id: token.id)}
+
+    it 'should belong to a userApp' do
+      expect(token.user_app).to eq(userApp)
+    end
+  end
 end
