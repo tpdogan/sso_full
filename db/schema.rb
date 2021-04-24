13f26a0ecdc9daa40b4539d01ea4448b4dabb481
tpdogan <pelintamerdogan@gmail.com>
2021-04-24 19:19:36 +0200

    token generation tests are added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_24_121447) do
+ActiveRecord::Schema.define(version: 2021_04_24_171257) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 2021_04_24_121447) do
     t.string "client_name"
     t.string "client_id"
     t.string "client_secret"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "tokens", force: :cascade do |t|
+    t.string "access_token"
+    t.string "token_type", default: "bearer"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
--- spec/models/token_spec.rb
+++ spec/models/token_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Token, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'generation tests' do
+    let (:token) {Token.create()}
+    it 'should generate a token' do
+      expect(token.access_token).not_to eq('')
+      expect(token.access_token).not_to eq(nil)
+    end
+
+    it 'should default type to bearer' do
+      expect(token.token_type).to eq('bearer')
+    end
+  end
 end
