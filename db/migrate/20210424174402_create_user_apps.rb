ba9c2c8d162f6d98d98cbedc2b4f279cda4f11dd
tpdogan <pelintamerdogan@gmail.com>
2021-04-25 00:26:03 +0200

    client id bug is fixed

--- db/migrate/20210424174402_create_user_apps.rb
+++ db/migrate/20210424174402_create_user_apps.rb
@@ -2,7 +2,7 @@ class CreateUserApps < ActiveRecord::Migration[6.1]
   def change
     create_table :user_apps do |t|
       t.integer :user_id
-      t.string :client_id
+      t.integer :client_id
       t.integer :auth_id
       t.integer :token_id
 
