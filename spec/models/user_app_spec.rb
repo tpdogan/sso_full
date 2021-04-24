9055d212512012193658b7b9e060e9c5706c5739
tpdogan <pelintamerdogan@gmail.com>
2021-04-25 00:52:41 +0200

    user app tests are updated for token and auth relations

--- spec/models/user_app_spec.rb
+++ spec/models/user_app_spec.rb
@@ -1,57 +1,51 @@
 require 'rails_helper'
 
 RSpec.describe UserApp, type: :model do
-  context 'validation tests' do
-    let (:user) { User.create(username: 'username', password: 'password') }
-    let (:client) { Client.create(client_name: 'client') }
-    let (:auth) {Auth.create()}
-    let (:token) {Token.create()}
+  let (:user) { User.create(username: 'username', password: 'password') }
+  let (:client) { Client.create(client_name: 'client') }
+  let (:userAppCreate) {UserApp.create(user_id: user.id, client_id: client.id)}
 
-    it 'should have a user id' do
+  context 'validation tests' do
+    it 'should have a client id' do
       userApp = UserApp.new(user_id: user.id).save
       expect(userApp).to eql(false)
     end
 
-    it 'should have a client id' do
+    it 'should have a user id' do
       userApp = UserApp.new(client_id: client.id).save
       expect(userApp).to eql(false)
     end
 
-    it 'should not save without auth_id or token_id' do
+    it 'should save with a user id and a client id' do
       userApp = UserApp.new(user_id: user.id, client_id: client.id).save
-      expect(userApp).to eql(false)
-    end
-
-    it 'should save with auth_id' do
-      userApp = UserApp.new(user_id: user.id, client_id: client.id, auth_id: auth.id).save
       expect(userApp).to eql(true)
     end
 
-    it 'should save with token_id' do
-      userApp = UserApp.new(user_id: user.id, client_id: client.id, token_id: token.id).save
-      expect(userApp).to eql(true)
+    it 'should be able to generate a auth' do
+      expect(userAppCreate.create_auth).to eql(userAppCreate.auth)
     end
 
-    it 'should not save with both auth_id and token_id' do
-      userApp = UserApp.new(user_id: user.id, client_id: client.id, auth_id: auth.id, token_id: token.id).save
-      expect(userApp).to eql(false)
+    it 'should be able to generate a token' do
+      expect(userAppCreate.create_token).to eql(userAppCreate.token)
+    end
+
+    it 'should not have both auth and token' do
+      expect(userAppCreate.create_auth).to eql(userAppCreate.auth)
+      expect(userAppCreate.token).to eq(nil)
+      expect(userAppCreate.create_token).to eql(userAppCreate.token)
+      expect(userAppCreate.auth).to eq(nil)
     end
   end
 
   context 'association tests' do
-    let (:user) { User.create(username: 'username', password: 'password') }
-    let (:client) { Client.create(client_name: 'client') }
-    let (:token) {Token.create()}
-    let (:userApp) {UserApp.create(user_id: user.id, client_id: client.id, token_id: token.id)}
-
     it 'should have a user' do
-      expect(userApp.user).not_to eq(nil)
-      expect(userApp.user).to eq(user)
+      expect(userAppCreate.user).not_to eq(nil)
+      expect(userAppCreate.user).to eq(user)
     end
 
     it 'should have a client' do
-      expect(userApp.client).not_to eq(nil)
-      expect(userApp.client).to eq(client)
+      expect(userAppCreate.client).not_to eq(nil)
+      expect(userAppCreate.client).to eq(client)
     end
   end
 end
