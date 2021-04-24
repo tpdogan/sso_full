cf78f080174c02db2565191951d8e1be0a695a97
tpdogan <pelintamerdogan@gmail.com>
2021-04-24 20:09:53 +0200

    userapps validation tests are added

--- spec/models/user_app_spec.rb
+++ spec/models/user_app_spec.rb
@@ -1,5 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe UserApp, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validation tests' do
+    let (:user) { User.create(username: 'username', password: 'password') }
+    let (:client) { Client.create(client_name: 'client') }
+    let (:auth) {Auth.create()}
+    let (:token) {Token.create()}
+
+    it 'should have a user id' do
+      userApp = UserApp.new(user_id: user.id).save
+      expect(userApp).to eql(false)
+    end
+
+    it 'should have a client id' do
+      userApp = UserApp.new(client_id: client.id).save
+      expect(userApp).to eql(false)
+    end
+
+    it 'should not save without auth_id or token_id' do
+      userApp = UserApp.new(user_id: user.id, client_id: client.id).save
+      expect(userApp).to eql(false)
+    end
+
+    it 'should save with auth_id' do
+      userApp = UserApp.new(user_id: user.id, client_id: client.id, auth_id: auth.id).save
+      expect(userApp).to eql(true)
+    end
+
+    it 'should save with token_id' do
+      userApp = UserApp.new(user_id: user.id, client_id: client.id, token_id: token.id).save
+      expect(userApp).to eql(true)
+    end
+
+    it 'should not save with both auth_id and token_id' do
+      userApp = UserApp.new(user_id: user.id, client_id: client.id, auth_id: auth.id, token_id: token.id).save
+      expect(userApp).to eql(false)
+    end
+  end
 end
