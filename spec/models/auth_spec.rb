f22d42e6ebbc4049b53deba0b9e3b4fe151ad1f3
tpdogan <pelintamerdogan@gmail.com>
2021-04-24 14:18:24 +0200

    auth generation test is added

--- spec/models/auth_spec.rb
+++ spec/models/auth_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Auth, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'generation tests' do
+    it 'should generate an auth_code' do
+      auth = Auth.new()
+      expect(auth.auth_code).not_to eq('')
+      expect(auth.auth_code).not_to eq(nil)
+    end
+  end
 end
