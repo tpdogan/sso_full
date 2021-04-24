2d3eb68b6f7b66aa055ab24322b87f7b080da778
tpdogan <pelintamerdogan@gmail.com>
2021-04-24 19:44:31 +0200

    userapp model is generated

---
+++ db/migrate/20210424174402_create_user_apps.rb
@@ -0,0 +1,12 @@
+class CreateUserApps < ActiveRecord::Migration[6.1]
+  def change
+    create_table :user_apps do |t|
+      t.integer :user_id
+      t.string :client_id
+      t.integer :auth_id
+      t.integer :token_id
+
+      t.timestamps
+    end
+  end
+end
