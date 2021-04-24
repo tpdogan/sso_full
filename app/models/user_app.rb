9b63704f18bedef2d1f20e8df98b9019069345fa
tpdogan <pelintamerdogan@gmail.com>
2021-04-24 20:10:19 +0200

    userapp validations are added and tested

--- app/models/user_app.rb
+++ app/models/user_app.rb
@@ -1,2 +1,11 @@
 class UserApp < ApplicationRecord
+  validates_presence_of :user_id, :client_id
+  validate :code_xor_token
+  
+  private
+  def code_xor_token
+    unless self.auth_id.present? ^ self.token_id.present?
+      errors.add(:base, 'Either auth_code or access_token must be present!')
+    end
+  end
 end
