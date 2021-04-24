1d6d71a668b8b84d0b7b40527750956bbcf047e5
tpdogan <pelintamerdogan@gmail.com>
2021-04-24 14:17:52 +0200

    auth table is migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_24_104134) do
+ActiveRecord::Schema.define(version: 2021_04_24_121447) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "auths", force: :cascade do |t|
+    t.string "auth_code"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "clients", force: :cascade do |t|
     t.string "client_name"
