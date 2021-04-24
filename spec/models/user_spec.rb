b9afad8cf775b738aa7d2e4e28bcd1e0ae16dee2
tpdogan <pelintamerdogan@gmail.com>
2021-04-24 12:38:13 +0200

    user validation tests are added

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validation tests' do
+    it 'should have a username' do
+      userSaved = User.new(password: 'password').save
+      expect(userSaved).to eq(false)
+    end
+
+    it 'should have a non-empty username' do
+      userSaved = User.new(username: '', password: 'password').save
+      expect(userSaved).to eq(false)
+    end
+
+    it 'should have a password' do
+      userSaved = User.new(username: 'username').save
+      expect(userSaved).to eq(false)
+    end
+
+    it 'should have a non-empty password' do
+      userSaved = User.new(username: 'username', password: '').save
+      expect(userSaved).to eq(false)
+    end
+
+    it 'should save successfully with credentials' do
+      userSaved = User.new(username: 'username', password: 'password').save
+      expect(userSaved).to eq(true)
+    end
+  end
 end
