6966cf6ba7c8e4ead68d633dbffd497c3305bc69
tpdogan <pelintamerdogan@gmail.com>
2021-04-24 13:26:58 +0200

    client generations are added and tested

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,3 +1,8 @@
 class Client < ApplicationRecord
   validates_presence_of :client_name
+
+  before_create do
+    self.client_id = SecureRandom.uuid
+    self.client_secret = SecureRandom.alphanumeric(40)
+  end
 end
