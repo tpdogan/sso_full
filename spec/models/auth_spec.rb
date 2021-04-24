b92a05b0cb72c8926b31422dc02b173189959ffe
tpdogan <pelintamerdogan@gmail.com>
2021-04-24 20:37:14 +0200

    association test is added for auth

--- spec/models/auth_spec.rb
+++ spec/models/auth_spec.rb
@@ -8,4 +8,15 @@ RSpec.describe Auth, type: :model do
       expect(auth.auth_code).not_to eq(nil)
     end
   end
+
+  context 'association tests' do
+    let (:user) { User.create(username: 'username', password: 'password') }
+    let (:client) { Client.create(client_name: 'client') }
+    let (:auth) {Auth.create()}
+    let (:userApp) {UserApp.create(user_id: user.id, client_id: client.id, auth_id: auth.id)}
+
+    it 'should belong to a userApp' do
+      expect(auth.user_app).to eq(userApp)
+    end
+  end
 end
