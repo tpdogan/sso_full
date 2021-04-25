04d740ce538e4906eade10c22cc157013594d824
tpdogan <pelintamerdogan@gmail.com>
2021-04-26 01:15:04 +0200

    login method is added to oauth and tested

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -25,9 +25,39 @@ class OauthController < ApplicationController
     end
   end
 
+  def login
+    user = User.find_by(login_params)
+
+    # Check if user does not exist
+    if !user
+      payload = {
+        error: "Username and/or password is not correct.",
+        status: 401
+      }
+      render json: payload, status: :unauthorized
+    # Check if it should not redirect after login
+    elsif !params[:redirect_uri]
+      payload = {
+        success: "Welcome #{user.username.capitalize}."
+      }
+      render json: payload, status: :ok
+    # Redirect user after successful login
+    else
+      redirect_to params[:redirect_uri]
+    end
+  end
+
   private
 
   def authorize_params
     params.permit(:client_id, :redirect_uri, :response_type)
+  end
+
+  def login_params
+    require "openssl"
+    key = "8a6f4407980a072017cef7204d70604051caff05c4f94f5778d09a0cda844494b775160dd9d1e8cc432df23090abaf1f8df9af93b6ed8105fb05ea990d596a95"
+    params[:user][:password] = OpenSSL::HMAC.hexdigest("sha256", key, params[:user][:password])
+
+    params.require(:user).permit(:username, :password)
   end
 end
