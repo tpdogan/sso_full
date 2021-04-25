4c889e38392a416e7d8fab950869e7b0329ada69
tpdogan <pelintamerdogan@gmail.com>
2021-04-25 20:18:14 +0200

    oauth authorize tests are added

--- spec/requests/oauth_spec.rb
+++ spec/requests/oauth_spec.rb
@@ -1,7 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe "Oauths", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  context "POST /authorize" do
+    it 'should return json content' do
+      post '/oauth/authorize'
+      expect(response.content_type).to eq("application/json; charset=utf-8")
+    end
+
+    it 'should return bad request' do
+      post '/oauth/authorize'
+      expect(response.bad_request?).to eq(true)
+    end
+
+    it 'should return forbidden access' do
+      post '/oauth/authorize', params: {client_id: 'random client id', redirect_uri: 'random uri', response_type: 'code'}
+      expect(response.bad_request?).to eq(false)
+      expect(response.forbidden?).to eq(true)
+    end
+
+    it 'should redirect to login page without session' do
+      client = Client.create(client_name: 'client')
+      post '/oauth/authorize', params: {client_id: client.client_id, redirect_uri: 'random uri', response_type: 'code'}
+      expect(response.status).to eq(302)
+      expect(response.redirect?).to eq(true)
+    end
   end
 end
