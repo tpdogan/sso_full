845e217b91817f99897b579dae2342e465730ac0
tpdogan <pelintamerdogan@gmail.com>
2021-04-26 13:22:04 +0200

    login path is not a member of oauth routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   resource :oauth, only: [] do
     member do
       post 'authorize' => 'oauth#authorize'
-      post 'login' => 'oauth#login'
     end
   end
   get '/login' => 'sessions#new'
