6aa4df7d44d81571234324fbfe6c0c587bbd8d3e
tpdogan <pelintamerdogan@gmail.com>
2021-04-26 12:10:37 +0200

    new session tests are added

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -1,7 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe "Sessions", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  context "GET /new" do
+    before(:each) { get '/login' }
+
+    it 'should respond successfully' do
+      expect(response.ok?).to eq(true)
+    end
+
+    it 'should generate an html page' do
+      expect(response.content_type).to eq('text/html; charset=utf-8')
+    end
   end
 end
